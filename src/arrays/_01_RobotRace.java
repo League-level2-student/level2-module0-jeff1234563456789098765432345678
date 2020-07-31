cf44d16232983e8a151f7ca13874208474c9797b
Shivam <shivam.pansuria@gmail.com>
2020-07-30 18:34:13 -0700

    ArrayCheatSheet,RobotRace :IP:

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,17 +1,36 @@
 package arrays;
 
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
 	//1. make a main method
+public static void main(String[] args) {
+	
 
 	//2. create an array of 5 robots.
-
+	Robot[] robots = new Robot[5]; 
 	//3. use a for loop to initialize the robots.
+	
+for (int i = 0; i < robots.length; i++) {
+robots[i] = new Robot(); 
+robots[i].setY(540);
+robots[i].setX(100+(i*100));
 
+	
+}
 		//4. make each robot start at the bottom of the screen, side by side, facing up
 
 	//5. use another for loop to iterate through the array and make each robot move 
 	//   a random amount less than 50.
-
+Robot[] robs = new Robot[50]; 
+for (int i = 0; i < robs.length; i++) {
+	Random rand = new Random();
+	int p = rand.nextInt(50);
+	robs[i].move(p);
+	
+}
 	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
 
 	//7. declare that robot the winner and throw it a party!
@@ -19,4 +38,5 @@ public class _01_RobotRace {
 	//8. try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
+}
 }
